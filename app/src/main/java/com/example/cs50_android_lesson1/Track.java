072fa2553d9f61ac1d68967b8f9e9c13faad38e2
yechubot <yechukim217@gmail.com>
2020-11-09 17:40:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/cs50_android_lesson1/Track.java
@@ -0,0 +1,12 @@
+package com.example.cs50_android_lesson1;
+
+public class Track {
+    String name;
+    String instructor;
+
+    public Track(String name, String instructor){
+        this.name = name;
+        this.instructor = instructor;
+
+    }
+}
